6abaaadebdb6eb074bc50f9f16d8790390d49329
Vasilis Ethan Sarris <vas82@pitt.edu>
2020-11-15 17:55:48 -0500

    added Price to meter list

--- ParkingPalApp/app/routes.py
+++ ParkingPalApp/app/routes.py
@@ -2,7 +2,8 @@ from app import app
 from calendar import day_name
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import FindParkingForm
-from app.meters import find_nearest_meters, get_geolocation, get_prediction, get_zone
+from app.meters import find_nearest_meters, get_geolocation, get_prediction, get_zone, get_price
+import datetime
 
 
 @app.route("/")
@@ -32,14 +33,16 @@ def map():
 
         close_meters = find_nearest_meters(user_location, radius)
         for meter in close_meters:
-            prediction = get_prediction({
+            prediction_info = {
                 'day_of_week': day_name[form.date.data.weekday()],
                 'is_holiday': str(False),
                 'meter': meter['id'],
                 'month': f'{form.date.data.month:02}',
                 'time': form.time.data.isoformat(),
                 'zone': get_zone(meter['id'])
-            })
+            }
+            prediction = get_prediction(prediction_info)
+            price = get_price(prediction_info)
             meter_availability.append(
-                {'id': meter['id'], 'lat': meter['lat'], 'lon': meter['lon'], 'prediction': prediction})
+                {'id': meter['id'], 'lat': meter['lat'], 'lon': meter['lon'], 'prediction': prediction, 'price': price})
     return render_template("map.html", form=form, user_location=user_location, zoom=zoom, meters=meter_availability)
